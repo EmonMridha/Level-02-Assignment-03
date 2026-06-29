65733905a7a9e7b5bb307cbeca907dc25cafe42b
EmonMridha <emonmridha712@gmail.com>
2026-06-29 19:28:59 +0600

    added first and secodn query

--- QUERY.sql
+++ QUERY.sql
@@ -159,3 +159,10 @@ INSERT INTO Bookings (user_id, match_id, seat_number, payment_status, total_cost
 --  Answers
 
 -- Query 1: Retrieve all upcoming football matches belonging to the 'Champions League' where the match status is 'Available'.
+
+select match_id,fixture,base_ticket_price from matches where tournament_category = 'Champions League' and 
+match_status = 'Available'
+
+-- Query 2: Search for all users whose full names start with 'Tanvir' or contain the phrase 'Haque' (case-insensitive)
+
+select * from users where full_name ilike 'Tanvir%' or full_name ilike '%haque%'
